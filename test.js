71c62d280067b5c460fb677d0337ea76bcec79e9
WebGreenTea <sahawut092013@gmail.com>
2021-04-18 17:42:31 +0700

    save

--- test.js
+++
@@ -1,9 +0,0 @@
-let today = new Date();
-let dd = String(today.getDate()).padStart(2, '0');
-let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-let yyyy = today.getFullYear();
-let hour = today.getHours();
-let min = today.getMinutes();
-let sec = today.getSeconds();
-today = mm + '/' + dd + '/' + yyyy + '|' + hour + '/' + min + '/' + sec;
-console.log(today)
